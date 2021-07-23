04d19f97ab08cc4fcab0c9798d4ac2d5655036e3
AAKASH <aakashaggarwal2311@gmail.com>
2021-07-23 18:40:13 +0530

    Fetching versions directly from local repo(modified get_versions) and added gitignore file

--- consecutive_versions_patch_comparator.py
+++ consecutive_versions_patch_comparator.py
@@ -48,15 +48,22 @@ def add_emptyfile(path,branch):
 
 #Function to clone single single branch from the repository and storing it making different folders
 
-def clone_version(all_version,repository_name):
-    print('Cloning versions........')
+def get_version(all_version,repo_local_name):
+    print('fetching versions........')
     for i in range(len(all_version)):
       brch=all_version[i]
-      path= os.getcwd()+'\\Fedora project'+'\\versions\\'+brch
+      destination_path= os.getcwd()+'\\versions\\'+brch
       # print(brch)
       #print(path)
-      Repo.clone_from(repository_name,path , branch=brch) # cloning single version creating a folder named as version name
-      add_emptyfile(path,brch)#adding an empty patch file in the folder
+      subprocess.run(['git','checkout', brch],cwd=os.getcwd()+'\\'+repo_local_name)
+      source_path=os.getcwd()+'\\'+repo_local_name
+    
+      version_patch_files=os.listdir(source_path)
+      shutil.copytree(source_path,destination_path)
+
+
+      #Repo.clone_from(repository_name,path , branch=brch) # cloning single version creating a folder named as version name
+      add_emptyfile(destination_path,brch)#adding an empty patch file in the folder
 
 #Function to compare consecutive versions
 
@@ -72,15 +79,15 @@ def consecutive_compare(branches):
 def compare(version1,version2):
   diff_dir_name=""+version1+version2
   folder_name="versions_diff"
-  diff_dir_parent_dir=os.getcwd()+"\\Fedora project\\"
-  if not os.path.exists(diff_dir_parent_dir+folder_name):
+  diff_dir_parent_dir=os.getcwd()
+  if not os.path.exists(diff_dir_parent_dir+"\\"+folder_name):
     os.mkdir(os.path.join(diff_dir_parent_dir,folder_name)) # creating a version diff folder whih will contain all diff folders(For eg f11f10)
-  diff_dir_parent_dir+=folder_name+'\\'
+  diff_dir_parent_dir+='\\'+folder_name
   diff_dir_path = os.path.join(diff_dir_parent_dir, diff_dir_name)
   os.mkdir(diff_dir_path)# creating diff folder(For eg f11f10)
 
-  path1=os.getcwd()+"\\Fedora project\\versions\\"+version1
-  path2=os.getcwd()+"\\Fedora project\\versions\\"+version2
+  path1=os.getcwd()+"\\versions\\"+version1
+  path2=os.getcwd()+"\\versions\\"+version2
   version1_patch_files={} # creating a dictionary which will keep a check of same patch files name among 2 versions
   for file in os.listdir(path1):
     if file.endswith(".patch"):
@@ -146,14 +153,14 @@ def count_lines(diff,path):
     elif(j[0]=='-'):
       deleted_lines+=1
     total_lines+=1
-  return added_lines,deleted_lines,total_lines;
+  return added_lines-1,deleted_lines-1,total_lines;
 
 #Initializing csv file
 
 def int_csv():
   fields=['path_file','branch1','branch2','added_lines','deleted_lines','is_deleted']
   csv_name="comparison_sheet.csv"
-  with open(os.path.join(os.getcwd()+'\\Fedora project',csv_name), 'w') as csvfile:
+  with open(os.path.join(os.getcwd(),csv_name), 'w') as csvfile:
     csvwriter = csv.writer(csvfile) 
     csvwriter.writerow(fields)
   return csv_name
@@ -162,7 +169,7 @@ def int_csv():
 #Function to fetch added lines, deleted lines,patch name and is deleted from a diff folder For eg f10f11 and insert it into excel sheet
 
 def check_changes(branch_diff,csv_name):
-  path=os.getcwd()+'\\Fedora project\\versions_diff'+'\\'+branch_diff
+  path=os.getcwd()+'\\versions_diff'+'\\'+branch_diff
   for diff in os.listdir(path):
     patch_name=str(diff)
     print(patch_name)
@@ -181,7 +188,7 @@ def check_changes(branch_diff,csv_name):
     isdeleted=0
     if(deleted_lines!=0 and added_lines<=1):
       isdeleted=1
-    with open(os.path.join(os.getcwd()+'\\Fedora project',csv_name), 'a') as csvfile: #writing the information into the csv for each diff file
+    with open(os.path.join(os.getcwd(),csv_name), 'a') as csvfile: #writing the information into the csv for each diff file
       csvwriter = csv.writer(csvfile)
       csvwriter.writerow([patch_name,branch1,branch2,added_lines,deleted_lines,isdeleted])
 
@@ -189,37 +196,44 @@ def check_changes(branch_diff,csv_name):
 
 def main():  
   parent_dir=os.getcwd()
-  os.mkdir(os.path.join(parent_dir,'Fedora project')) #creating fedora project folder
+  print(parent_dir)
+  #os.mkdir(os.path.join(parent_dir,'Fedora project')) #creating fedora project folder
   folder_name="versions"
-  parent_dir=parent_dir+'\\'+'Fedora project'
-  if not os.path.exists(parent_dir+folder_name):
+  #parent_dir=parent_dir+'\\'+'Fedora project'
+  if not os.path.exists(os.getcwd()+folder_name):
     os.mkdir(os.path.join(parent_dir,folder_name)) #creating versions folder
   #parent_dir+=folder_name+'/'
-
+  
   all_branches=[] #will store all branches names
 
   repository_name=input('Enter the repository git link:')
   repository_name=str(repository_name)
-  subprocess.run(['git', 'clone',repository_name],cwd=parent_dir )# cloning the repository inside the Fedora project folder
-  stdout = subprocess.check_output('git branch -r'.split(),cwd=parent_dir+'\\grub2',) #fetching all branches names
+  rep_local_name="Fedora_repo"
+  subprocess.run(['git', 'clone',repository_name,rep_local_name],cwd=parent_dir )# cloning the repository inside the Fedora project folder
+
+  stdout = subprocess.check_output('git branch -r'.split(),cwd=parent_dir+'\\'+ rep_local_name,) #fetching all branches names
   out = stdout.decode()
   all_branches = [b.strip('* ') for b in out.splitlines()]
   #print(all_branches)
-  all_branches.sort()
+  #all_branches.sort()
 
   all_version=fetch_versions_name(all_branches) #fetching version names from the branches
   print('Version:\n')
   print(all_version)
 
-  clone_version(all_version,repository_name)#cloning versions seperately from the repository 
-  branches = os.listdir(os.getcwd()+'\\Fedora project\\versions')#fetching all the versions from the versions folder
+  print(os.getcwd())
+
+  
+  get_version(all_version,rep_local_name)#cloning versions seperately from the repository 
+  
+  branches = os.listdir(os.getcwd()+'\\versions')#fetching all the versions from the versions folder
   branches.sort()
 
   consecutive_compare(branches) # function to compare consecutive versions
 
   csv_name=int_csv() #initializing csv file with the headers
 
-  diff_folders=os.listdir(os.getcwd()+'\\Fedora project\\versions_diff') #storing all diff folders
+  diff_folders=os.listdir(os.getcwd()+'\\versions_diff') #storing all diff folders
   diff_folders.sort()
   for i in range( len(diff_folders) ):
     check_changes(diff_folders[i],csv_name) #checking changes in each diff file inside a folder
